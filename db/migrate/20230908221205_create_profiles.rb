39dabcdca4916b806993def86ec804ed7ad512bf
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-09-08 20:29:46 -0300

    refactor: include modifiers

--- db/migrate/20230908221205_create_profiles.rb
+++ db/migrate/20230908221205_create_profiles.rb
@@ -1,10 +1,10 @@
 class CreateProfiles < ActiveRecord::Migration[7.0]
   def change
     create_table :profiles do |t|
-      t.string :name
-      t.string :email
-      t.date :birthdate
-      t.string :phone, limit: 11
+      t.string :name, null: false, limit: 255
+      t.string :email, null: false, limit: 255
+      t.date :birthdate, null: false
+      t.string :phone, null:false, limit: 11
       t.string :links, array: true, default: []
       t.string :role
 
