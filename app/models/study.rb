b9f1b24de6f6dfcc63bba6967646ca76ca7f39cb
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-09-08 21:07:17 -0300

    feat: add validates

--- app/models/study.rb
+++ app/models/study.rb
@@ -1,3 +1,10 @@
 class Study < ApplicationRecord
   belongs_to :profile
+
+  validates :profile_id, presence: true
+  validates :title, :start_date, presence: true
+  validates :title, length: { maximum: 255 }
+  validates :start_date, :end_date, date: true
+  validates :end_date, date: { after_or_equal_to: :start_date }
+
 end
