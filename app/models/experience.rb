0c710192e0f8462b91a40480841afed5b3482300
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-09-08 21:03:39 -0300

    feat: add validates

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,3 +1,10 @@
 class Experience < ApplicationRecord
   belongs_to :profile
+
+  validates :title, :company_name, :start_date, :function_performed, presence: true
+  validates :title, :company_name, length: { maximum: 255 }
+  validates :function_performed, length: { maximum: 500 }
+  validates :start_date, :end_date, date: true
+  validates :end_date, date: { after_to: :start_date }, allow_blank: true
+
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,13 @@
+# frozen_string_literal: true
+
 class Profile < ApplicationRecord
+  has_many :experiences, dependent: :destroy
+
+  validates :name, :email, :birthdate, :phone, presence: true
+  validates :name, length: { maximum: 255 }
+  validates :email, length: { maximum: 255 }
+  validates :phone, length: { maximum: 11 }
+  validates :role, inclusion: { in: %w[frontend backend fullstack mobile designer qa] }
 
   enum :role, {
     frontend: 1,
