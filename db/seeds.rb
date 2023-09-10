00b55d8f1176bc535c09d5e45f3640f87da49929
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-09-10 14:12:26 -0300

    feat: add gem faker and generate data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+def generate_links
+  links = []
+  rand(1..5).times do
+    links << Faker::Internet.url
+  end
+  links
+end
+
+def random_role
+  roles = %w[frontend backend fullstack mobile designer qa]
+  roles.sample
+end
+
+puts 'Inserting profiles...'
+10.times do
+  profile = Profile.find_or_create_by(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    birthdate: Faker::Date.birthday(min_age: 18, max_age: 65),
+    phone: Faker::Number.number(digits: 11)
+  )
+
+  profile.update(links: generate_links)
+  profile.update(role: random_role)
+end
+puts 'Finished inserting profiles!'
