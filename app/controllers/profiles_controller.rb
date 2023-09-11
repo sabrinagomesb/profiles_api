3e3b0f2b2250cfe3a4c64a902d326739ea1745fb
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-09-11 10:37:51 -0300

    feat: add params and render for Experience and Study

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,18 +1,22 @@
 class ProfilesController < ApplicationController
   def index
     @profiles = Profile.all
-    render json: @profiles
+    render json: @profiles, include: %i[experiences studies]
   end
 
   def show
     @profile = Profile.find(params[:id])
-    render json: @profile
+    render json: @profile, include: %i[experiences studies]
   end
 
   def create
-    @profile = Profile.new(profile_params)
+    @profile = Profile.new(profile_params.except(:studies_attributes))
 
     if @profile.save
+      studies_attributes = profile_params[:studies_attributes]
+      studies_attributes.each { |study| study[:profile_id] = @profile.id }
+
+      @profile.studies.create(studies_attributes)
       render json: @profile, status: :created
     else
       render json: @profile.errors, status: :unprocessable_entity
@@ -22,6 +26,10 @@ class ProfilesController < ApplicationController
   private
 
   def profile_params
-    params.require(:profile).permit(:name, :email, :birthdate, :phone, :links, :role, :bio)
+    params.require(:profile).permit(
+      :name, :email, :birthdate, :phone, :links, :role, :bio,
+      experiences_attributes: %i[title company_name start_date end_date function_performed _destroy],
+      studies_attributes: %i[title start_date end_date _destroy]
+    )
   end
 end
