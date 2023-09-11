0f3bea32fd7520b3cb437ace237eac5086b0c213
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-09-11 10:36:42 -0300

    feat: add acepted nested attr para Experience and Study

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,9 +2,12 @@
 
 class Profile < ApplicationRecord
   has_many :experiences, dependent: :destroy
-  has_many :studies, dependent: :destroy
+  has_many :studies, dependent: :destroy, inverse_of: :profile
   has_many :abilities, through: :profile_abilities
   has_many :techs, through: :profile_techs
+
+  accepts_nested_attributes_for :experiences, allow_destroy: true
+  accepts_nested_attributes_for :studies, allow_destroy: true
 
   validates :name, :email, :birthdate, :phone, presence: true
   validates :name, length: { maximum: 255 }
