d3be108f52e914d97f95e60563926b04f1741e9b
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-09-10 22:05:02 -0300

    refactor: delete unnecessary validates

--- app/models/profile_ability.rb
+++ app/models/profile_ability.rb
@@ -4,14 +4,4 @@ class ProfileAbility < ApplicationRecord
 
   validates :profile, :ability, presence: true
   validates :profile, uniqueness: { scope: :ability }
-  validates :ability, inclusion: { in: %w[frontend backend fullstack mobile designer qa] }
-
-  enum :type, {
-    frontend: 1,
-    backend: 2,
-    fullstack: 3,
-    mobile: 4,
-    designer: 5,
-    qa: 6
-  }
 end
