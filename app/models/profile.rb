61ee6c4b7408ac4310715dcd2a9494dfd93ad425
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-09-10 22:04:05 -0300

    feat: add info relations to Abilities and Techs

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,19 +3,21 @@
 class Profile < ApplicationRecord
   has_many :experiences, dependent: :destroy
   has_many :studies, dependent: :destroy
+  has_many :abilities, through: :profile_abilities
+  has_many :techs, through: :profile_techs
 
   validates :name, :email, :birthdate, :phone, presence: true
   validates :name, length: { maximum: 255 }
   validates :email, length: { maximum: 255 }
-  validates :phone, length: { maximum: 11 }
+  validates :phone, length: { is: 11 }
   validates :role, inclusion: { in: %w[frontend backend fullstack mobile designer qa] }
 
   enum :role, {
-    frontend: 1,
-    backend: 2,
-    fullstack: 3,
-    mobile: 4,
-    designer: 5,
-    qa: 6
+    frontend: 'frontend',
+    backend: 'backend',
+    fullstack: 'fullstack',
+    mobile: 'mobile',
+    designer: 'designer',
+    qa: 'qa'
   }
 end
