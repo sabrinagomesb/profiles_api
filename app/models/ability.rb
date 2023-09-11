b0b7c02fe08e7b913f3970a5cdb3388c79633d2b
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-09-10 22:03:03 -0300

    feat: add validates

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,5 +1,6 @@
 class Ability < ApplicationRecord
-
   validates :name, presence: true, uniqueness: true
   validates :name, length: { maximum: 255 }
+
+  validates :role, inclusion: { in: Profile.roles.values }
 end
