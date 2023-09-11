0c7e81ddff4d75a048e519af58ac6c8cb8a1147b
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-09-10 22:02:24 -0300

    feat: generate data to tables Abilities and Techs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
+require_relative 'seeds_constants'
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+roles = Profile.roles.values
 
 def generate_links
   links = []
@@ -16,21 +11,31 @@ def generate_links
   links
 end
 
-def random_role
-  roles = %w[frontend backend fullstack mobile designer qa]
-  roles.sample
-end
-
 puts 'Inserting profiles...'
 10.times do
-  profile = Profile.find_or_create_by(
+  Profile.find_or_create_by(
     name: Faker::Name.name,
+    bio: Faker::Lorem.paragraph,
     email: Faker::Internet.email,
     birthdate: Faker::Date.birthday(min_age: 18, max_age: 65),
-    phone: Faker::Number.number(digits: 11)
+    phone: Faker::Number.number(digits: 11),
+    links: generate_links,
+    role: roles.sample
   )
-
-  profile.update(links: generate_links)
-  profile.update(role: random_role)
 end
+
 puts 'Finished inserting profiles!'
+
+puts 'Inserting techonologies...'
+
+STACKS.each do |tech|
+  Tech.find_or_create_by(name: tech)
+end
+
+puts 'Finished inserting techonologies!'
+
+puts 'Inserting abilities...'
+
+FRONTEND_ABILITIES.each do |skill|
+  Ability.find_or_create_by(name: skill[:name], role: Profile.roles[:frontend], description: skill[:description])
+end
