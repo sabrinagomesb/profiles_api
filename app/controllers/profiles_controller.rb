438cf00b62501d38713bd5d9e272327b220b93cf
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-09-12 16:24:47 -0300

    feat: permit params to ability e tech ids

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,12 +1,12 @@
 class ProfilesController < ApplicationController
   def index
     @profiles = Profile.all
-    render json: @profiles, include: %i[experiences studies]
+    render json: @profiles, include: %i[experiences studies abilities techs]
   end
 
   def show
     @profile = Profile.find(params[:id])
-    render json: @profile, include: %i[experiences studies]
+    render json: @profile, include: %i[experiences studies abilities techs]
   end
 
   def create
@@ -15,9 +15,9 @@ class ProfilesController < ApplicationController
     if @profile.save
       studies_attributes = profile_params[:studies_attributes]
       studies_attributes.each { |study| study[:profile_id] = @profile.id }
-
       @profile.studies.create(studies_attributes)
-      render json: @profile, status: :created
+
+      render json: @profile, include: %i[experiences studies abilities techs], status: :created
     else
       render json: @profile.errors, status: :unprocessable_entity
     end
@@ -29,7 +29,9 @@ class ProfilesController < ApplicationController
     params.require(:profile).permit(
       :name, :email, :birthdate, :phone, :links, :role, :bio,
       experiences_attributes: %i[title company_name start_date end_date function_performed _destroy],
-      studies_attributes: %i[title start_date end_date _destroy]
+      studies_attributes: %i[title start_date end_date _destroy],
+      ability_ids: [],
+      tech_ids: []
     )
   end
 end
