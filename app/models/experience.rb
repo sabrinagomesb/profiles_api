16ec2daa99fc4173c923ae86e3fbfd0b5321a263
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-09-12 22:38:31 -0300

    refactor: remove date validations

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -4,7 +4,4 @@ class Experience < ApplicationRecord
   validates :title, :company_name, :start_date, :function_performed, presence: true
   validates :title, :company_name, length: { maximum: 255 }
   validates :function_performed, length: { maximum: 500 }
-  validates :start_date, :end_date, date: true
-  validates :end_date, date: { after_to: :start_date }, allow_blank: true
-
 end
