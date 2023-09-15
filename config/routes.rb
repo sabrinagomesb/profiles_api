3207af42d4d1dac1ce5515f33018fc2c599ffe9f
Larissa Rabelo <larissarabelolf@gmail.com>
2023-09-14 21:11:18 -0300

    feat: add show routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+
   resources :profiles, only: %i[create show index]
-
   resources :teches, only: %i[index show], path: 'techs'
-
-  get '/abilities', to: 'abilities#index'
+  resources :abilities, only: %i[show index]
 end
