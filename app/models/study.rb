bc94cd6ee6556f0c5680b1a0cece639c3f744cec
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-09-15 17:02:13 -0300

    refactor: remove duplicate line

--- app/models/study.rb
+++ app/models/study.rb
@@ -2,7 +2,6 @@ class Study < ApplicationRecord
   belongs_to :profile, inverse_of: :studies
 
   validates :title, :institution, presence: true
-  validates :title, :institution, presence: true
   validates :title, length: { maximum: 255 }
   validates :institution, length: { minimum: 4, maximum: 255 }
   validates :start_date, format: { with: /\A\d{4}-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|3[01])\z/, message: 'must be in the format YYYY-MM-DD' }, allow_blank: true
